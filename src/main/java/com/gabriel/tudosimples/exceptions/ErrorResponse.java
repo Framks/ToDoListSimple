0c877f98868f9e85e5abab6e74c1229ee78a5b33
Francisco Gabriel <galvesa9@gmail.com>
2024-02-05 15:36:54 -0300

    adcionando tratamendo de exceções personalizadas.

---
+++ src/main/java/com/gabriel/tudosimples/exceptions/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.gabriel.tudosimples.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    private String stackTrace;
+    private List<ValidationError> errors;
+
+    @RequiredArgsConstructor
+    @Getter
+    @Setter
+    private static class ValidationError{
+        private final String field;
+        private final String message;
+    }
+
+    public void addValitadionError(String field, String message){
+        if (Objects.isNull(errors)){
+            this.errors = new ArrayList<>();
+        }
+        this.errors.add(new ValidationError(field,message));
+    }
+}
---
+++ src/main/java/com/gabriel/tudosimples/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.gabriel.tudosimples.services.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND) // 404
+public class ObjectNotFoundException extends EntityNotFoundException {
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+}
